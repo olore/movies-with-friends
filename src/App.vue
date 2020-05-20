9af7b4db352ccec592e7ede3af00589df6a75919
Brian Olore <brian@olore.net>
2020-05-20 11:26:08 -0400

    source material design icons locally, trying to make bundle smaller

--- src/App.vue
+++ src/App.vue
@@ -57,9 +57,5 @@ export default {
   data: () => ({
     drawer: null,
   }),
-
-  created() {
-    this.$vuetify.theme.dark = true;
-  },
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import minifyTheme from "minify-css-string";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: "mdi",
+  },
+  theme: {
+    // from https://vuetifyjs.com/en/customization/theme/
+    dark: true,
+    options: { minifyTheme },
+  },
+});
