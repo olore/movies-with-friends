1cc7d1699278b03fe824718d27d66fb921d3a96d
Brian Olore <brian@olore.net>
2020-05-19 21:53:18 -0400

    ender cards!

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,77 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      clipped-left
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-row >
+        <v-col cols="12" sm="4">
+          <Card tile class="ma-3 pa-6"/>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <Card tile class="ma-3 pa-6"/>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <Card tile class="ma-3 pa-6"/>
+        </v-col>
+      </v-row>
+
     </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import Card from './components/Card';
 
-export default {
-  name: 'App',
+  export default {
+    components: {
+      Card,
+    },
 
-  components: {
-    HelloWorld,
-  },
+    props: {
+      source: String,
+    },
 
-  data: () => ({
-    //
-  }),
-};
+    data: () => ({
+      drawer: null,
+    }),
+
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://m.media-amazon.com/images/M/MV5BMjAzMzI5OTgzMl5BMl5BanBnXkFtZTgwMTU5MTAwMDE@._V1_SX300.jpg" 
+    >
+      <v-card-title></v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">Ender's Game</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>6.6/10 | 62%</div>
+
+      <div>
+        Young Ender Wiggin is recruited by the International Military to lead the fight against the Formics, 
+        an insectoid alien race who had previously tried to invade Earth and had inflicted heavy losses on humankind.
+      </div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
