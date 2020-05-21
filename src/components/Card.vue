b64782c55043f08d231ef8df7c0f9c8bf77dbb87
Brian Olore <brian@olore.net>
2020-05-21 09:40:44 -0400

    use a movie object

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,41 +1,34 @@
 <template>
   <v-card class="mx-auto" max-width="300">
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      src="https://m.media-amazon.com/images/M/MV5BMjAzMzI5OTgzMl5BMl5BanBnXkFtZTgwMTU5MTAwMDE@._V1_SX300.jpg"
-    >
+    <v-img class="white--text align-end" height="200px" :src="movie.image">
       <v-card-title></v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pb-0">Ender's Game</v-card-subtitle>
+    <v-card-subtitle class="pb-0">{{ movie.title }}</v-card-subtitle>
 
     <v-card-text class="text--primary">
       <div
         class="d-inline-flex align-self-stretch justify-center"
         style="width: 100%;"
       >
-        <IMDBRating rating="6.6" imdbid="tt1731141" class="mr-3"></IMDBRating>
+        <IMDBRating
+          :rating="movie.imdbRating"
+          :imdbid="movie.imdbid"
+          class="mr-3"
+        ></IMDBRating>
         <RottenTomatoesRating
-          rating="62%"
-          title="Ender's game"
+          :rating="movie.tomatoesRating"
+          :title="movie.title"
         ></RottenTomatoesRating>
       </div>
 
       <div>
-        Young Ender Wiggin is recruited by the International Military to lead
-        the fight against the Formics...
-        <!-- an insectoid alien race who had previously tried to invade Earth and had inflicted heavy losses on humankind. -->
+        {{ movie.description }}
       </div>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn
-        color="orange"
-        text
-        target="_blank"
-        href="https://reelgood.com/movie/enders-game-2013"
-      >
+      <v-btn color="orange" text target="_blank" :href="reelGoodLink()">
         Where to watch?
       </v-btn>
 
@@ -50,9 +43,33 @@ import IMDBRating from "./IMDBRating";
 import RottenTomatoesRating from "./RottenTomatoesRating";
 export default {
   name: "Card",
+  data: () => ({
+    movie: {
+      title: "Ender's Game",
+      description:
+        "Young Ender Wiggin is recruited by the International Military to lead the fight against the Formics...",
+      //an insectoid alien race who had previously tried to invade Earth and had inflicted heavy losses on humankind.
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMjAzMzI5OTgzMl5BMl5BanBnXkFtZTgwMTU5MTAwMDE@._V1_SX300.jpg",
+      imdbRating: "6.6",
+      tomatoesRating: "62%",
+      imdbid: "tt1731141",
+      year: "2013",
+    },
+  }),
   components: {
     IMDBRating,
     RottenTomatoesRating,
+  },
+  methods: {
+    reelGoodLink: function () {
+      return (
+        "https://reelgood.com/movie/" +
+        this.movie.title.replace(/ /, "-").replace("'", "") +
+        "-" +
+        this.movie.year
+      );
+    },
   },
 };
 </script>
