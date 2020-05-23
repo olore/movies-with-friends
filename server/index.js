f5139703b73de180cac20cfc2dcc30dd6fb64126
Brian Olore <brian@olore.net>
2020-05-23 18:46:27 -0400

    listen more than 127.0.0.1

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ fastify.get("/movie", async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(3000, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
