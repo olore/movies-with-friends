81e330fb31b763a7622fc48891858965b4119e90
Brian Olore <brian@olore.net>
2020-08-27 10:16:08 -0400

    splash page updates

--- src/components/pages/SplashPage.vue
+++ src/components/pages/SplashPage.vue
@@ -2,19 +2,20 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <h3>MovieRex</h3>
+        <span class="text-subtitle-1 font-weight-light">MovieRex</span>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12" class="d-flex justify-center">
-        <h1>Your Friends. Your Family. Movies. Television.</h1>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" class="d-flex justify-center">
-        <v-btn class="primary" x-large to="/recently-rated"
-          >Launch MovieRex</v-btn
-        >
+      <v-col cols="12">
+        <div class="text-center">
+          <h1 class="text-h5 text-md-h4 pb-2">Find something new to watch</h1>
+          <v-btn class="primary" x-large to="/recently-rated"
+            >Launch MovieRex</v-btn
+          >
+          <p class="pt-2 text-md-h5">
+            See recommendations from friends and family
+          </p>
+        </div>
       </v-col>
     </v-row>
     <v-row>
@@ -53,6 +54,51 @@
             max-width="800"
             src="terminator-review.png"
           />
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="">
+        <div class="mt-16">
+          <div class="font-weight-thin">
+            Special thanks to
+          </div>
+          <ul class="font-weight-light">
+            <li><a href="https://vuejs.org/">VueJS</a> JavaScript framework</li>
+            <li>
+              <a href="https://vuetifyjs.com/">Vuetify</a> component framework
+            </li>
+            <li>
+              <a href="https://www.fastify.io/"> Fastify.io</a> Node.js server
+              side framework (no express!)
+            </li>
+            <li>
+              <a href="https://www.omdbapi.com/">Open Movie Data Base API</a>
+            </li>
+            <li>
+              <a href="https://github.com/louischatriot/nedb">NeDB</a> in
+              memory/file based document store
+            </li>
+            <li>
+              <a
+                href="https://github.com/mejiamanuel57/vue-google-signin-button-directive)"
+                >Google Signin Button</a
+              >
+              inspiration"
+            </li>
+            <li>
+              <a href="https://peachscript.github.io/vue-infinite-loading/"
+                >Infinite Loader</a
+              >
+              component
+            </li>
+            <li>
+              <a
+                href="http://www.iconarchive.com/show/captiva-icons-by-bokehlicia/movie-icon.html"
+                >Favicon</a
+              >
+            </li>
+          </ul>
         </div>
       </v-col>
     </v-row>
