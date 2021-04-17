20b992cc4ec69a6eb425067d8b18988db421e8d5
Brian Olore <brian@olore.net>
2021-04-16 22:00:21 -0400

    use node v14: no longer need to checkout package-lock.json

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,6 +5,5 @@ PATH=$PATH:$HOME/.nvm/versions/node/v8.11.2/bin
 nvm use default
 
 cd $HOME/apps/movies-with-friends
-git checkout -- package-lock.json # what's up with this?
 git pull
 npm ci && npm run build
