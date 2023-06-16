505887fd7eac2edd3b3fa84f5603b2f91a4d8afc
Brian Olore <brian@olore.net>
2023-06-15 20:45:15 -0400

    small fixes: running in dev and update ratings script (#224)
    * small fixes: running in dev and update ratings script
    
    * increase threshold to make the tests pass
    
    * try updating snapshots: Expected: 0.3 / Actual: 0.3423291281607975
    
    * fix: wait for app to start before running cypress
    
    * regression failing on GHA but not locally :/
    
    * put back some changes, I don't think they are necessary
    
    * revert changes that aren't needed
    
    * Revert "revert changes that aren't needed"
    
    This reverts commit 8a68af19b8bb7861aec667caa6740adedaf61449.

--- scripts/cy-regression.sh
+++ scripts/cy-regression.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-./node_modules/.bin/wait-on http://localhost:8080
-./node_modules/.bin/cypress run --env type=actual
+./node_modules/.bin/wait-on http-get://localhost:8080 && ./node_modules/.bin/cypress run --env type=actual
